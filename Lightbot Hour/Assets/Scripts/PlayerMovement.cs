7eaac1052f76e3ecaa9c5de3555b1fd33b17d21e
Soheil Hoseini <soheil.hosseini78@gmail.com>
2022-07-29 20:11:19 +0430

    Replace game objects with their parent prefab and change design.

--- Lightbot Hour/Assets/Scripts/PlayerMovement.cs
+++ Lightbot Hour/Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] List<MoveActions> inputActions = new List<MoveActions>();
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-
-    }
+    [SerializeField] List<GameObject> LevelsContainer = new List<GameObject>();
+    [SerializeField] float speed;
 
     public enum MoveActions
     {
@@ -24,5 +16,26 @@ public class PlayerMovement : MonoBehaviour
         RotateLeft,
         RotateRight
     }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        //transform.Translate(transform.right * speed * Time.deltaTime);
+        transform.position += transform.forward * Time.deltaTime * speed;
+        Debug.LogWarning(transform.forward.ToString());
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "wall")
+        {
+            Debug.Log("Hit the wall");
+            speed = 0; // Stop the player to avoid it from shaking
+        }
+    }
+
 
 }
