656fe8b2888cec334706dd6f90dd07d988ab8d57
Soheil Hoseini <soheil.hosseini78@gmail.com>
2022-07-31 13:03:23 +0430

    Movement and rotation

--- Lightbot Hour/Assets/Scripts/PlayerMovement.cs
+++ Lightbot Hour/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] List<MoveActions> inputActions = new List<MoveActions>();
     [SerializeField] List<GameObject> LevelsContainer = new List<GameObject>();
     [SerializeField] float speed;
-
+    
+    // There are five different moving actions
     public enum MoveActions
     {
         Forward,
@@ -24,18 +26,86 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         //transform.Translate(transform.right * speed * Time.deltaTime);
-        transform.position += transform.forward * Time.deltaTime * speed;
-        Debug.LogWarning(transform.forward.ToString());
-    }
+        //Debug.LogWarning(transform.forward.ToString());
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.tag == "wall")
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Hit the wall");
-            speed = 0; // Stop the player to avoid it from shaking
+            MoveForward();
+        }
+
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            MoveBackward();
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            TurnLeft();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            TurnRight();
         }
     }
 
+    private void MoveBackward()
+    {
+        transform.position -= transform.forward * speed;
+    }
 
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            Debug.Log("Hit the wall");
+            speed = 0; // Stop the player to avoid it from shaking
+            gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+        }
+
+        // Keep the player from flowing in the air when it hits the normal cubes
+        if (collision.gameObject.tag == "NormalCube")
+        {
+            Debug.Log("Hit Normal Cube");
+            gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
+        }
+    }
+
+    // To make the player move forward
+    public void MoveForward()
+    {
+        Debug.Log("Initial Position: " + transform.position.ToString());
+        transform.position += transform.forward * speed;
+        Debug.Log("Final Position: " + transform.position.ToString());
+    }
+
+    // To make the player turn 90 degrees counter-clockwise
+    public void TurnLeft()
+    {
+        transform.Rotate(0,-90,0);
+        Debug.Log("Rotate Left!");
+    }
+
+    // To make the player turn 90 degrees clockwise
+    public void TurnRight()
+    {
+        transform.Rotate(0, 90, 0);
+        Debug.Log("Rotate Right!");
+    }
+
+    // To make the player jump up and forward(simultaneously)
+    public void Jump()
+    {
+
+    }
+
+    // When the player gets to a goal cube, it can light up and go to the next state
+    public void LightUp()
+    {
+
+    }
 }
